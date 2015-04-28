08d50cfff2620cf2c60bc39e623d8fbf774eef05
Juan Navarrete <jmstyle008@gmail.com>
2015-04-28 14:43:28 -0500

    New and improved

--- gameOfLife.php
+++ gameOfLife.php
@@ -10,7 +10,10 @@ abstract class makeGame
 
     abstract public function getLifeArray();
 
-    abstract public function setLifeArray($array);
+    public function setLifeArray($array)
+    {
+        $this->lifeArray = (empty($array)) ? $this->makeLife() : $array;
+    }
 
     public function __construct()
     {
@@ -19,61 +22,47 @@ abstract class makeGame
 
     public function makeLife()
     {
-        for ($i = 0; $i < $this->xAxis; $i++) {
-            for ($j = 0; $j < $this->yAxis; $j++) {
+        for ($i = 0; $i < $this->xAxis; $i++)
+            for ($j = 0; $j < $this->yAxis; $j++)
                 $this->lifeArray[$i][$j] = rand(0, 1);
-            }
-        }
     }
 
     protected function rules()
     {
         $this->newLifeArray = $this->lifeArray;
-        for ($i = 0; $i < $this->xAxis; $i++) {
-            for ($j = 0; $j < $this->yAxis; $j++) {
+        for ($i = 0; $i < $this->xAxis; $i++)
+            for ($j = 0; $j < $this->yAxis; $j++)
                 $this->_validateRules($i, $j);
-            }
-        }
+    }
+
+    private function _checkNeighbors($i, $j)
+    {
+        $aliveNeighbors = 0;
+        for ($limitXInf = $i - 1; $limitXInf <= $i + 1; $limitXInf++)
+            for ($limitYInf = $j - 1; $limitYInf <= $j + 1; $limitYInf++)
+                $aliveNeighbors += ((bool)$this->lifeArray[$limitXInf][$limitYInf]) ? 1 : 0;
+        return $aliveNeighbors;
     }
 
     private function _validateRules($i, $j)
     {
-        $aliveNeighbors = 0;
-        for ($limitXInf = $i - 1; $limitXInf <= $i + 1; $limitXInf++) {
-            for ($limitYInf = $j - 1; $limitYInf <= $j + 1; $limitYInf++) {
-                $value = (int)$this->lifeArray[$limitXInf][$limitYInf];
-                if ((int)$value == 1)
-                    $aliveNeighbors += 1;
-            }
-        }
-        $currentValue = (int)$this->lifeArray[$i][$j];
-        if ($currentValue == 1) {
-            $aliveNeighbors = $aliveNeighbors - 1; //SO I WON'T COUNT MYSELF AS A NEIGHBOR
-        }
-        if ($aliveNeighbors < 2 && $currentValue == 1) { //UNDERPOPULATION = DEATH
+        $aliveNeighbors = ((bool)$this->lifeArray[$i][$j]) ? $this->_checkNeighbors($i, $j) - 1 : $this->_checkNeighbors($i, $j);
+        if ($aliveNeighbors > 3 || $aliveNeighbors < 2 && (bool)$this->lifeArray[$i][$j])  //LONELINESS || UNDERPOPULATION = DEATH
             $this->newLifeArray[$i][$j] = 0;
-        }
-        if (($aliveNeighbors == 2 || $aliveNeighbors == 3) && $currentValue == 1) {//IF WE HAVE 2 OR 3 ALIVE I LIVE!
+        if (($aliveNeighbors == 2 || $aliveNeighbors == 3) && (bool)$this->lifeArray[$i][$j]) //IF WE HAVE 2 OR 3 ALIVE I LIVE!
             $this->newLifeArray[$i][$j] = 1;
-        }
-        if ($aliveNeighbors > 3 && $currentValue == 1) { // IF GREATER THAN 3 ALIVE, OVER POPULATION, I'M DEAD
-            $this->newLifeArray[$i][$j] = 0;
-        }
-        if ($aliveNeighbors == 3 && $currentValue == 0) { //IF 3 ALIVE AND I'M DEAD, I LIVE AGAIN
+        if ($aliveNeighbors == 3 && !(bool)$this->lifeArray[$i][$j])  //IF 3 ALIVE AND I'M DEAD, I LIVE AGAIN
             $this->newLifeArray[$i][$j] = 1;
-        }
     }
 
-    protected function displayHtml()
+    public function displayLifeArray()
     {
-        $result = '<table >';
+        $result = '<table>';
         for ($i = 0; $i < $this->xAxis; $i++) {
             $result .= '<tr>';
             for ($j = 0; $j < $this->yAxis; $j++) {
-                $color = 'white';
-                if ((bool)$this->newLifeArray[$i][$j])
-                    $color = 'black';
-                $result .= '<td style="background-color:' . $color . '; color:' . $color . ';">TTTT<input type="hidden" name=array[' . $i . '][' . $j . '] value="' . $this->newLifeArray[$i][$j] . '"></td>';
+                $color = (bool)$this->newLifeArray[$i][$j] ? 'black' : 'white';
+                $result .= '<td style="background-color:' . $color . '; color:' . $color . ';">**<input type="hidden" name=array[' . $i . '][' . $j . '] value="' . $this->newLifeArray[$i][$j] . '"></td>';
             }
             $result .= '</tr>';
         }
@@ -83,17 +72,6 @@ abstract class makeGame
 
 class gameOfLife extends makeGame
 {
-    private function _startGame()
-    {
-        $this->rules();
-    }
-
-    public function displayLifeArray()
-    {
-
-        return $this->displayHtml();
-    }
-
     public function getLifeArray()
     {
         return $this->lifeArray;
@@ -101,17 +79,13 @@ class gameOfLife extends makeGame
 
     public function setLifeArray($array)
     {
-        $this->lifeArray = $array;
-        if (empty($array)) {
-            $this->makeLife();
-        }
-        $this->_startGame();
+        parent::setLifeArray($array);
+        $this->rules();
     }
 }
 
 $startGame = new gameOfLife();
 $startGame->setLifeArray($startGame->getLifeArray());
-if (array_key_exists('array', $_POST) && !empty($_POST['array'])) {
+if (array_key_exists('array', $_POST) && !empty($_POST['array']))
     $startGame->setLifeArray($_POST['array']);
-}
-echo json_encode(array('table' => $startGame->displayLifeArray(false)));
+echo json_encode(array('table' => $startGame->displayLifeArray()));
